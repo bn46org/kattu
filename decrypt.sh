1a1c7f7dae25a7ac497d370605bf31d0f0356f3a
bn46 <147074924+bn46@users.noreply.github.com>
2024-10-18 12:42:15 +0530

    Create decrypt.sh
    Signed-off-by: bn46 <147074924+bn46@users.noreply.github.com>

---
+++ decrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# decrypt-gpg-key.sh
+
+set -e
+
+# Ensure the environment variable GPG_PASSPHRASE is set
+if [ -z "$GPG_PASSPHRASE" ]; then
+  echo "Error: GPG_PASSPHRASE is not set."
+  exit 1
+fi
+
+# Decrypt the GPG key
+gpg --batch --yes --passphrase "$GPG_PASSPHRASE" -o user_map.json -d keys/user_map.json.gpg
